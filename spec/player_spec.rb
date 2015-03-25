f9f33039c939ccbdb4f3fc9e9e085d431293528f
Mark Pokorny <iridium193@gmail.com>
2015-03-25 16:01:52 +0000

    Added unit tests to player to check that it can load and retain ships.

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,4 +4,10 @@ describe Player do
   it 'has a board' do
     expect(subject.board).to be_a Board
   end
+
+  it 'has ships' do
+    ship = double :ship
+    subject.load_ship(ship)
+    expect(subject.fleet).to include ship
+  end
 end
