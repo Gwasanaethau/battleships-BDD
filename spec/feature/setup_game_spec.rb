6e9768251f731bffa1b210ba5a1877e6c8df37cd
Mark Pokorny <iridium193@gmail.com>
2015-03-25 12:17:13 +0000

    Written feature tests to set up the game.

---
+++ spec/feature/setup_game_spec.rb
@@ -0,0 +1,13 @@
+feature 'players can set up the game' do
+  xscenario 'a player needs a board'
+  xscenario 'a player has a range of ship sizes'
+  xscenario 'board is bigger than all the ships'
+  xscenario 'players can place ships on the board in a certain direction'
+  xscenario 'the ships must not overlap'
+end
+
+# xfeature 'the players can play the game'
+#
+# xfeature 'the players can win the game'
+#
+# xfeature 'more than one player can play the game'
